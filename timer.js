abfa5b1648b0ca5f6d991a7c6644f77981e2dbdc
Zoryana Makeeva <zoryana.dev@gmail.com>
2018-02-04 20:56:16 +0200

    first commit

---
+++ timer.js
@@ -0,0 +1,30 @@
+function startTimer(duration, display) {
+    var start = Date.now(),
+        delta,
+        seconds,
+        minutes;
+
+    function timer() {
+        delta = duration - (Math.round((Date.now() - start) / 1000));
+
+        minutes = Math.floor(delta / 60);
+        seconds = Math.floor(delta % 60);
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds;
+
+        if (delta <= 0) {
+            start = Date.now() + 1000;
+        }
+    }
+
+    timer();
+    (minutes !== 0 && seconds !== 0) ? setInterval(timer, 1000) : clearInterval(timer);
+}
+
+window.onload = function () {
+    var duration = (+prompt("на сколько минут запустить таймер?") * 60);
+    var display = document.getElementById("timer");
+    startTimer(duration, display);
+};
